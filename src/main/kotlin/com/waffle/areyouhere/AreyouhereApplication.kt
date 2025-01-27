118d3f18cd329cb64e9e3cea53d316b67168c5a3
SHEOMM <esh0821@snu.ac.kr>
2025-01-27 16:12:08 +0900

    First Commit

---
+++ src/main/kotlin/com/waffle/areyouhere/AreyouhereApplication.kt
@@ -0,0 +1,11 @@
+package com.waffle.areyouhere
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AreyouhereApplication
+
+fun main(args: Array<String>) {
+	runApplication<AreyouhereApplication>(*args)
+}
---
+++ src/test/kotlin/com/waffle/areyouhere/AreyouhereApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.waffle.areyouhere
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AreyouhereApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
