75fed4279f03b8fe70bbef29ce0603f1787e362e
SHEOMM <esh0821@snu.ac.kr>
2025-01-27 23:57:56 +0900

    feat: H2DB Connection Pool 설정

--- src/main/kotlin/com/waffle/areyouhere/config/database/H2DBConnectionPool.kt
+++ src/main/kotlin/com/waffle/areyouhere/config/database/H2DBConnectionPool.kt
@@ -1,3 +1,29 @@
 package com.waffle.areyouhere.config.database
 
-class H2DBConnectionPool
+import io.vertx.core.Vertx
+import io.vertx.jdbcclient.JDBCConnectOptions
+import io.vertx.jdbcclient.JDBCPool
+import io.vertx.sqlclient.Pool
+import io.vertx.sqlclient.PoolOptions
+import io.vertx.sqlclient.SqlConnectOptions
+import org.hibernate.reactive.pool.impl.DefaultSqlClientPool
+import java.net.URI
+
+class H2DBConnectionPool : DefaultSqlClientPool() {
+    override fun createPool(
+        uri: URI,
+        connectOptions: SqlConnectOptions,
+        poolOptions: PoolOptions,
+        vertx: Vertx,
+    ): Pool {
+        return JDBCPool.pool(
+            vertx,
+            JDBCConnectOptions()
+                .setJdbcUrl(connectOptions.host)
+                .setUser(connectOptions.user)
+                .setPassword(connectOptions.password)
+                .setDatabase(connectOptions.database),
+            poolOptions,
+        )
+    }
+}
