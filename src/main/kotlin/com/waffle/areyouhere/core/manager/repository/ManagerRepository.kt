9760b3959bf5f99da605b00bbd67a6ee32131939
Ikjun Choi <1350adwx@gmail.com>
2025-01-30 01:02:16 +0900

    id를 가진 entity에 대해 작동하는 base repository 제작 (#2)
    * feat: id를 가진 entity에 대해 작동하는 base repository 제작
    
    * feat: findAll, existsById, count 추가

--- src/main/kotlin/com/waffle/areyouhere/core/manager/repository/ManagerRepository.kt
+++ src/main/kotlin/com/waffle/areyouhere/core/manager/repository/ManagerRepository.kt
@@ -2,9 +2,9 @@ package com.waffle.areyouhere.core.manager.repository
 
 import com.linecorp.kotlinjdsl.dsl.jpql.jpql
 import com.linecorp.kotlinjdsl.render.jpql.JpqlRenderContext
-import com.linecorp.kotlinjdsl.support.hibernate.reactive.extension.createMutationQuery
 import com.linecorp.kotlinjdsl.support.hibernate.reactive.extension.createQuery
 import com.waffle.areyouhere.core.manager.model.Manager
+import com.waffle.areyouhere.util.BaseRepository
 import com.waffle.areyouhere.util.CustomJpql
 import io.smallrye.mutiny.coroutines.awaitSuspending
 import org.hibernate.reactive.mutiny.Mutiny.SessionFactory
@@ -13,20 +13,9 @@ import org.springframework.stereotype.Repository
 // FIXME: 구현 상 세션이 매번 새로 열린다.
 @Repository
 class ManagerRepository(
-    private val sessionFactory: SessionFactory,
-    private val context: JpqlRenderContext,
-) {
-
-    suspend fun findById(id: Long): Manager {
-        val query = jpql(CustomJpql) {
-            selectFrom(Manager::class)
-                .where(path(Manager::id).eq(id))
-        }
-        return sessionFactory.withSession {
-            it.createQuery(query, context).setMaxResults(1).singleResult
-        }.awaitSuspending()
-    }
-
+    override val sessionFactory: SessionFactory,
+    override val context: JpqlRenderContext,
+) : BaseRepository<Manager, Long>(sessionFactory, context, Manager::class, Manager::id) {
     // TODO: exists extension function 만들어서 효율좋게 해보기.
     suspend fun findByEmail(email: String): Manager? {
         val query = jpql(CustomJpql) {
@@ -36,21 +25,5 @@ class ManagerRepository(
         return sessionFactory.withSession {
             it.createQuery(query, context).setMaxResults(1).resultList
         }.awaitSuspending().firstOrNull()
-    }
-
-    suspend fun save(manager: Manager): Manager {
-        return sessionFactory.withTransaction { session, tx ->
-            session.persist(manager).chain(session::flush).replaceWith(manager)
-        }.awaitSuspending()
-    }
-
-    suspend fun deleteById(id: Long) {
-        val query = jpql(CustomJpql) {
-            deleteFrom(entity(Manager::class))
-                .where(path(Manager::id).eq(id))
-        }
-        sessionFactory.withTransaction { session, tx ->
-            session.createMutationQuery(query, context).executeUpdate()
-        }.awaitSuspending()
     }
 }
