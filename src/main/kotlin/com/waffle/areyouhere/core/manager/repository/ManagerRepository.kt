2693ddf459fe2262fb3f241d7fa4eef5a3e21cd6
SHEOMM <esh0821@snu.ac.kr>
2025-01-29 18:50:06 +0900

    fix: repository 계층은 코루틴 방식으로 통일

--- src/main/kotlin/com/waffle/areyouhere/core/manager/repository/ManagerRepository.kt
+++ src/main/kotlin/com/waffle/areyouhere/core/manager/repository/ManagerRepository.kt
@@ -2,13 +2,14 @@ package com.waffle.areyouhere.core.manager.repository
 
 import com.linecorp.kotlinjdsl.dsl.jpql.jpql
 import com.linecorp.kotlinjdsl.render.jpql.JpqlRenderContext
+import com.linecorp.kotlinjdsl.support.hibernate.reactive.extension.createMutationQuery
 import com.linecorp.kotlinjdsl.support.hibernate.reactive.extension.createQuery
 import com.waffle.areyouhere.core.manager.model.Manager
-import com.waffle.areyouhere.core.util.CustomJpql
+import com.waffle.areyouhere.util.CustomJpql
 import io.smallrye.mutiny.converters.uni.UniReactorConverters
+import kotlinx.coroutines.reactor.awaitSingle
 import org.hibernate.reactive.mutiny.Mutiny.SessionFactory
 import org.springframework.stereotype.Repository
-import reactor.core.publisher.Mono
 
 @Repository
 class ManagerRepository(
@@ -16,13 +17,32 @@ class ManagerRepository(
     private val context: JpqlRenderContext,
 ) {
 
-    fun findById(id: Long): Mono<Manager> {
+    suspend fun findById(id: Long): Manager {
         val query = jpql(CustomJpql) {
             selectFrom(Manager::class)
                 .where(path(Manager::id).eq(id))
         }
-        return sessionFactory.withSession {
-            it.createQuery(query, context).setMaxResults(1).singleResult
+        return sessionFactory.withTransaction { session ->
+            session.createQuery(query, context).setMaxResults(1).singleResult
         }.convert().with(UniReactorConverters.toMono())
+            .awaitSingle()
+    }
+
+    suspend fun save(manager: Manager): Manager {
+        return sessionFactory.withTransaction { session ->
+            session.persist(manager).chain(session::flush).replaceWith(manager)
+        }.convert().with(UniReactorConverters.toMono())
+            .awaitSingle()
+    }
+
+    suspend fun deleteById(id: Long): Int? {
+        val query = jpql(CustomJpql) {
+            deleteFrom(entity(Manager::class))
+                .where(path(Manager::id).eq(id))
+        }
+        return sessionFactory.withTransaction { session, tx ->
+            session.createMutationQuery(query, context).executeUpdate()
+        }.convert().with(UniReactorConverters.toMono())
+            .awaitSingle()
     }
 }
--- src/test/kotlin/com/waffle/areyouhere/core/manager/repository/ManagerRepositoryTest.kt
+++ src/test/kotlin/com/waffle/areyouhere/core/manager/repository/ManagerRepositoryTest.kt
@@ -1,10 +1,11 @@
 package com.waffle.areyouhere.core.manager.repository
 
-import kotlinx.coroutines.reactor.awaitSingleOrNull
+import com.waffle.areyouhere.core.manager.model.Manager
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.assertEquals
 
 @SpringBootTest
 class ManagerRepositoryTest {
@@ -15,9 +16,15 @@ class ManagerRepositoryTest {
     @Test
     fun `레포지토리 첫 테스트`() {
         runBlocking {
-            managerRepository.findById(123L).awaitSingleOrNull()?.let {
-                println("Found manager with id ${it.id}: ${it.name}, ${it.email}, ${it.password}")
-            }
+            val manager = Manager(
+                email = "hellp@naver.com",
+                name = "seongho",
+                password = "hi",
+            )
+            val saveManager = managerRepository.save(manager)
+            val foundManager = managerRepository.findById(manager.id!!)
+            println("hello" + foundManager.id)
+            assertEquals(saveManager.id, foundManager.id)
         }
     }
 }
